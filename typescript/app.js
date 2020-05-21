7a6d0138852152255ec5e354e4cde706da33c09c
Gabriel Fernandes <gfernandes410@gmail.com>
2020-05-20 23:04:21 -0300

    update

--- typescript/app.js
+++ typescript/app.js
@@ -7,3 +7,10 @@ var massage2 = LoggerAndReturn(2);
 console.log(massageWithString);
 console.log(massageWithOutString);
 console.log(massage2);
+function getArray(items) {
+    return new Array().concat(items);
+}
+var myNumArr = getArray([100, 200, 300]);
+var myStrArr = getArray(['Hello', 'Word']);
+console.log(myNumArr);
+console.log(myNumArr);
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,3 +1,5 @@
+// ------------------------------------------------------------
+// ---------- seção 2.5----------------------------------------
 function LoggerAndReturn<T>(thing: T) : T {
     return thing
 }
@@ -6,7 +8,19 @@ const massageWithString: string = LoggerAndReturn<string>('massageWithString Wor
 const massageWithOutString: string = LoggerAndReturn('massageWithOutString Word!')
 const massage2: number = LoggerAndReturn(2)
 
+// console.log(massageWithString)
+// console.log(massageWithOutString)
+// console.log(massage2)
 
-console.log(massageWithString)
-console.log(massageWithOutString)
-console.log(massage2)
+// ------------------------------------------------------------
+// ---------- seção 2.6----------------------------------------
+
+function getArray<T>(items: T[]): T[] {
+    return new Array<T>().concat(items)
+}
+
+let myNumArr = getArray<number>([100,200,300])
+let myStrArr = getArray<string>(['Hello','Word'])
+
+console.log(myNumArr)
+console.log(myNumArr)
